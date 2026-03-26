c22a7a62e74b0c9ee3590451496ebd4689396c31
roseewood <193557890+roseewood@users.noreply.github.com>
2026-03-26 18:37:59 +0530

    added transaction route file

---
+++ backend/routes/transactionRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getTransactions,
+  addTransaction,
+  deleteTransaction,
+} = require("../controllers/transactionController");
+
+router.get("/", getTransactions);
+router.post("/", addTransaction);
+router.delete("/:id", deleteTransaction);
+
+module.exports = router;

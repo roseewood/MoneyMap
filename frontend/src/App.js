82374c270663caadc8a8190f0d0281029bd05961
roseewood <193557890+roseewood@users.noreply.github.com>
2026-03-28 17:39:21 +0530

    added Api.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div>
+      <h1>Personal Finance Manager</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;

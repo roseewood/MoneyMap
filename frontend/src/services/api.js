e12f638706a9dee45257fb0e64d24c07f3bc2739
roseewood <193557890+roseewood@users.noreply.github.com>
2026-03-30 12:11:56 +0530

    updated api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,14 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
+
 export const getTransactions = () => API.get("/transactions");
 export const addTransaction = (data) => API.post("/transactions", data);
 export const deleteTransaction = (id) =>
   API.delete(`/transactions/${id}`);
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) req.headers.authorization = token;
+  return req;
+});
